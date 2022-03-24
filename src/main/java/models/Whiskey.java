8071bf18340374d5a21d1cbb2acefc2ccbd8dd6e
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-03-24 19:30:25 -0400

    added fields to sneaker

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Whiskey {
+    private int id;
+    private String name;
+    private String brand;
+    private String sport;
+    private int size;
+    private int qty;
+    private float price;
 }
