633a361b8faf850e7fdb3e9e0a04f31a9982538c
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-03-24 22:05:33 -0400

    finished section 2

--- src/main/java/services/WhiskeyService.java
+++ src/main/java/services/WhiskeyService.java
@@ -1,4 +1,42 @@
 package services;
 
+import models.Sneaker;
+import models.Whiskey;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WhiskeyService {
+    private int nextId = 1;
+
+    private List<Whiskey> inventory = new ArrayList<>();
+
+    public Whiskey create(String name, String brand, double alcConcentration, double size, double price, int qty) {
+        Whiskey createdWhiskey = new Whiskey(nextId++, name, brand, alcConcentration, size, price, qty);
+        inventory.add(createdWhiskey);
+        return createdWhiskey;
+    }
+
+    public Whiskey findWhiskey(int id) {
+        for (Whiskey s : inventory) {
+            if (s.getId() == id)
+                return s;
+        }
+        return null;
+    }
+
+    public Whiskey[] findAll() {
+        return inventory.toArray(new Whiskey[inventory.size()]);
+    }
+
+    public boolean delete(int id) {
+        for (Whiskey s : inventory) {
+            if (s.getId() == id) {
+                inventory.remove(s);
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
--- src/test/java/services/WhiskeyServiceTest.java
+++ src/test/java/services/WhiskeyServiceTest.java
@@ -1,7 +1,60 @@
 package services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import models.Whiskey;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class WhiskeyServiceTest {
+    @Test
+    void createTest() {
+        String expectedName = "Stan Smith";
+        String expectedBrand = "Adidas";
+        Double expectedAlc = 0.30;
+        double expectedSize = 1.5;
+        double expectedPrice = 80.00f;
+        int expectedQty = 10;
+        WhiskeyService whiskeyService = new WhiskeyService();
+        Whiskey testWhiskey = whiskeyService.create(expectedName, expectedBrand, expectedAlc, expectedSize, expectedPrice, expectedQty);
 
+        int actualId = testWhiskey.getId();
+        String actualName = testWhiskey.getName();
+        String actualBrand = testWhiskey.getBrand();
+        double actualAlc = testWhiskey.getAlcConcentration();
+        double actualSize = testWhiskey.getSize();
+        int actualQty = testWhiskey.getQty();
+        double actualPrice = testWhiskey.getPrice();
+
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedBrand, actualBrand);
+        Assertions.assertEquals(expectedAlc, actualAlc);
+        Assertions.assertEquals(expectedSize, actualSize);
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+    @Test
+    void findTest(){
+        WhiskeyService whiskeyService = new WhiskeyService();
+        whiskeyService.create("1","",0,0.0,0,1);
+        whiskeyService.create("2","",0,0.0,0,1);
+    }
+
+    @Test
+    void findAllTest(){
+        WhiskeyService whiskeyService = new WhiskeyService();
+        whiskeyService.create("1","",0,0.0,0,1);
+        whiskeyService.create("2","",0,0.0,0,1);
+        Whiskey[] whiskies = whiskeyService.findAll();
+        Assertions.assertEquals(2,whiskies.length);
+    }
+
+    @Test
+    void deleteTest(){
+        WhiskeyService whiskeyService = new WhiskeyService();
+        whiskeyService.create("1","",0,0.0,0,1);
+        whiskeyService.create("2","",0,0.0,0,1);
+
+        Assertions.assertTrue(whiskeyService.delete(2));
+        Assertions.assertFalse(whiskeyService.delete(3));
+    }
 }
