3bb1f3daf987d09e7944efeab372cbb4ec59b81e
JD Singer <jdsinger@Zipcoders-MacBook-Pro-5.local>
2022-08-01 21:18:16 -0400

    hockey stick tests are done and functioning

--- src/test/java/models/HockeyStickTests.java
+++ src/test/java/models/HockeyStickTests.java
@@ -18,18 +18,65 @@ public class HockeyStickTests {
     @Test
     public void HockeyStickNameTest(){
         //given
-        String expectedName = "bauer";
-
+        String expectedName = "rbz";
         //when
         stick.setName(expectedName);
         String actualName = stick.getName();
-
-
         //then
-
         Assertions.assertEquals(expectedName, actualName);
+    }
 
+    @Test
+    public void HockeyStickBrand(){
+        String expectedBrand = "CCM";
+
+        stick.setBrand(expectedBrand);
+        String actualBrand = stick.getBrand();
+
+        Assertions.assertEquals(expectedBrand, actualBrand);
+    }
+
+    @Test
+    public void HockeyStickSize(){
+        Integer expectedSize = 64;
+
+        stick.setSize(expectedSize);
+        Integer actualSize = stick.getSize();
+
+        Assertions.assertEquals(expectedSize,actualSize);
+    }
+
+    @Test
+    public void HockeyStickQuantity(){
+        Integer expectedQty = 3;
+
+        stick.setQty(expectedQty);
+        Integer actualQty = stick.getQty();
+
+        Assertions.assertEquals(expectedQty,actualQty);
+    }
+
+    @Test
+    public void HockeyStickPrice(){
+        Double expectedPrice = 170.00;
+
+        stick.setPrice(expectedPrice);
+        Double actualPrice = stick.getPrice();
+
+        Assertions.assertEquals(expectedPrice,actualPrice);
+    }
+
+    @Test
+    public void HockeyStickId(){
+        Integer expectedId = 2424;
+
+        stick.setId(expectedId);
+        Integer actualId = stick.getId();
+
+        Assertions.assertEquals(expectedId,actualId);
 
     }
+
+
 
 }
