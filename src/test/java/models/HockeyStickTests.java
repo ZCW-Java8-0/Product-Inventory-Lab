ac300fa809783e9897ef35734c1c71f2d021ba6e
JD Singer <jdsinger@Zipcoders-MacBook-Pro-5.local>
2022-08-01 20:17:33 -0400

    started into tests

--- src/test/java/models/HockeyStickTests.java
+++ src/test/java/models/HockeyStickTests.java
@@ -1,4 +1,35 @@
 package models;
 
+import com.sun.xml.internal.ws.policy.AssertionSet;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class HockeyStickTests {
+
+    private HockeyStick stick;
+
+    @BeforeEach
+    public void setup(){
+        stick = new HockeyStick();
+    }
+
+    @Test
+    public void HockeyStickNameTest(){
+        //given
+        String expectedName = "bauer";
+
+        //when
+        stick.setName(expectedName);
+        String actualName = stick.getName();
+
+
+        //then
+
+        Assertions.assertEquals(expectedName, actualName);
+
+
+    }
+
 }
